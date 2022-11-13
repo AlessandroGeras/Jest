02a2717c3b797966b9afec6a047b3ccf6aabb06f
Alessandro Geras <alessandrogeras@gmail.com>
2022-11-13 14:23:08 -0300

    Teste de Função

--- src/jest.test.js
+++ src/jest.test.js
@@ -1,7 +1,7 @@
 const math = require("./math");
 const { sum } = math();
 
-describe("Teste de função simples math.js", () => {
+describe("Teste de função simples - math.js", () => {
   test("A soma esperada de -4 e 1 deve ser -3", () => {
     expect(sum(-4, 1)).toBe(-3);
   });
